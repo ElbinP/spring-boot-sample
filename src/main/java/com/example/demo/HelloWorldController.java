6e8e1ed0bae7af0460fe68de5f8b9719c78a2c88
Elbin <elbin.p@gmail.com>
2022-03-26 11:41:19 +0530

    update port and path

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demo")
 public class HelloWorldController {
 
 	@GetMapping("/hello")
 	public String getMessage() {
 		return "Hello World";
+	}
+
+	@GetMapping(value = "/healthcheck", produces = { MediaType.APPLICATION_JSON_VALUE })
+	public String health() {
+		return "success";
 	}
 
 }
